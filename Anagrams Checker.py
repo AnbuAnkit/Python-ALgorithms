c36cc013b6336f293f6b1d10954b65b201613bdc
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-01 16:19:02 +0530

    Update Anagrams Checker.py

--- Anagrams Checker.py
+++ Anagrams Checker.py
@@ -1,1 +1,36 @@
 
+word1 = str(input("Please enter a word:"))
+word2 = str(input("Please enter another word:"))
+
+def checker(str1, str2):
+    global x
+    global f
+
+    x = 0
+    f = 0
+
+    global word1
+    global word2
+
+    word1 = word1.upper()
+    word2 = word2.upper()
+
+    string1 = sorted(word1)
+    string2 = sorted(word2)
+
+    if (len(string1)
+            != len(string2)):
+        print("False")
+    else:
+
+        while x < len(string1) - 1:
+            if string1[x] != string2[x]:
+                f += 1
+            x += 1
+
+        if f > 0:
+            print("False")
+        else:
+            print("True")
+
+checker(word1, word2)
