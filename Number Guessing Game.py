1930040e9c41e2655f901bdcd48c7b4f0a9d54b5
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-02 17:15:42 +0530

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,32 +1,52 @@
 import random
 
-num1 = 40
-num2 = 50
-r = random.randint(num1, num2)
+def range():
 
-print("Range =", num1, "to", num2)
-response = int(input("Guess your number between the given range:"))
+    global num1
+    global num2
 
-def game(r1, r2):
-    global response
-    global r
-    c = 3
+    num1 = int(input("Please enter a initial number:"))
+    num2 = int(input("Please enter a final number:"))
 
-    if response < r1 or response > r2:
-        print("Please enter a number within the range...")
+    if num2 == num1 + 1:
+        print("Range cant be this short.")
+        range()
     else:
-        while c > 1:
-            if response != r:
+        print("You've got 3 chances to guess the correct answer, Good luck!")
+range()
+
+r = random.randint(num1 + 1, num2 - 1)
+c = 2
+
+def guessingGame():
+
+    global num1
+    global num2
+    global r
+    global c
+
+    print("Range =", num1, "to", num2)
+    response = int(input("Guess your number between the given range:"))
+
+    if num1 > num2:
+        print("Enter a valid range (Intial limit cannot be higher then Final limit)")
+    else:
+        if response < num1 or response > num2:
+            print("Enter a value between the given range.")
+            guessingGame()
+        else:
+            if c == 0:
+                print("Game over! Better luck next time.")
+            elif c == 1:
+                print("LAST CHANCE!")
                 c -= 1
-                print("Wrong answer, try again!")
-                game(r1, r2)
+                guessingGame()
             else:
-                print("Your answer is correct!")
+                if response != r:
+                    print("Wrong!")
+                    c -= 1
+                    guessingGame()
+                else:
+                    print("You won! Your answer is correct.")
 
-    if c <= 0:
-        print("Sorry but you have lost the game... better luck next time!")
-
-game(num1, num2)
-
-#print("Your range is", num1, "to", num2)
-
+guessingGame()
