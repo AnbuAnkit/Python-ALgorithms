fe5c3a2e34dfaa7bff91cfe9692daf279aad9f64
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-02 19:35:25 +0530

    Update Word Guessing Game.py

--- Word Guessing Game.py
+++ Word Guessing Game.py
@@ -4,10 +4,31 @@ name = "Ankit"
 
 l = []
 r = random.randint(0, len(name) - 1)
+c = 4
 
 for i in name:
     l.append(i)
+
 randomLetter = l[r]
 
-print(randomLetter)
+def loop():
+    global c
+    global response
 
+    response = str(input("Please enter a letter:"))
+
+    if c == 0:
+        print("Game over! Better luck next time.")
+    elif c == 1:
+        print("Last Chance!")
+        c -= 1
+        loop()
+    else:
+        if response != randomLetter:
+            c -= 1
+            print("Wrong!")
+            loop()
+        else:
+            print("You won! Your answer is correct.")
+
+loop()
