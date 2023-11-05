d6cd1e986c044b652ae5fc035dcd795340fa06b4
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-05 17:06:12 +0530

    Update Hangman Game.py

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,10 +1,17 @@
 import time
+import re
 
-string = "ankit"
+def checker():
+    string = str(input("Please enter your target word:"))
+
+    if strin
+
+
+
 string = string.upper()
 
 x = 0
-c = 3
+c = 2
 
 l = []
 for i in string:
@@ -16,23 +23,23 @@ while x < len(l) - 2:
     x += 1
 dash = "".join(dash)
 
-print("Guess the correct word! \nYour hint is given below:")
+print("Guess the correct word! you've got 3 chances. \nYour hint is:")
 print(l[0], dash, l[len(l) - 1], sep="")
 
-#time.sleep(3)
-
-response = str(input("Enter your answer:"))
-response = response.upper()
+time.sleep(1)
 
 def loop():
     global c
+
+    response = str(input("Enter your answer:"))
+    response = response.upper()
 
     if c == 0:
         print("Game over! Better luck next time!")
     else:
         if response != string:
-            print("Wrong!")
-            c += 1
+            print("WRONG! Chances remaining:", c)
+            c -= 1
             loop()
         else:
             print("You won! your answer was correct.")
