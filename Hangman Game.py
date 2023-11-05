a1aa870d3ae10d277b6c06f935a13836cf7addfd
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-05 16:58:38 +0530

    Update Hangman Game.py

--- Hangman Game.py
+++ Hangman Game.py
@@ -4,6 +4,8 @@ string = "ankit"
 string = string.upper()
 
 x = 0
+c = 3
+
 l = []
 for i in string:
     l.append(i)
@@ -17,6 +19,21 @@ dash = "".join(dash)
 print("Guess the correct word! \nYour hint is given below:")
 print(l[0], dash, l[len(l) - 1], sep="")
 
-time.sleep(3)
+#time.sleep(3)
 
 response = str(input("Enter your answer:"))
+response = response.upper()
+
+def loop():
+    global c
+
+    if c == 0:
+        print("Game over! Better luck next time!")
+    else:
+        if response != string:
+            print("Wrong!")
+            c += 1
+            loop()
+        else:
+            print("You won! your answer was correct.")
+loop()
