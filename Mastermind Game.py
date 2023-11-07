56a2da4c366a33aa9bde74442d3db31088af3571
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-07 16:43:44 +0530

    Update Mastermind Game.py

--- Mastermind Game.py
+++ Mastermind Game.py
@@ -1,25 +1,35 @@
 #INITIANL INPUT
-def checker():
+def intialInput():
     global plr1
     global plr2
 
     plr1 = 348
     if plr1 < 100 or plr1 > 999:
         print("Please enter a number given by the spacified range.")
-        checker()
+        intialInput()
 
     plr2 = 456
     if plr2 < 100 or plr2 > 999:
         print("Please enter a number given by the specified range.")
-        checker()
-checker()
+        intialInput()
+intialInput()
 #########
 
 #CODE UNDER DEVELOPMENT#
 
-#DASH CONSTRUCTOR
 
-if
+
+#DASH CONSTRUCTOR
+parentDash = []
+
+stringPlr1 = str(plr1)
+
+for i in stringPlr1:
+    parentDash.append(i)
+dash = [int(i) for i in parentDash]
+#########
+
+
 
 #CHECKER FUNCTION
 if plr2 == plr1:
@@ -28,3 +38,4 @@ else:
     print("Wrong!")
     print("Here your hint, try again.")
 
+#EXPERIMENT#
