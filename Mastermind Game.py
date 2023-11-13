6226051cec9dd78128686c9e36a9c7d057d74bca
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-13 19:14:42 +0530

    Update Mastermind Game.py

--- Mastermind Game.py
+++ Mastermind Game.py
@@ -5,7 +5,7 @@ def intialInput():
 
     plr1 = 348
     if plr1 < 100 or plr1 > 999:
-        print("Please enter a number given by the spacified range.")
+        print("Please enter a number given by the specified range.")
         intialInput()
 intialInput()
 #########
@@ -18,7 +18,6 @@ dash = []
 
 for i in strPlr1:
     dash.append(i)
-
 #########
 
 
@@ -42,6 +41,51 @@ def checker():
         if plr2 < 100 or plr2 > 999:
             print("Please enter a number given by the specified range.")
             plr2input()
+    plr2input()#INITIANL INPUT
+def intialInput():
+    global plr1
+    global plr2
+
+    plr1 = 348
+    if plr1 < 100 or plr1 > 999:
+        print("Please enter a number given by the spacified range.")
+        intialInput()
+intialInput()
+#########
+
+
+
+#DASH CONSTRUCTOR
+strPlr1 = str(plr1)
+dash = []
+dupliDash = []
+
+for i in strPlr1:
+    dash.append(i)
+    dupliDash.append(i)
+#########
+
+
+
+#CHECKER FUNCTION
+plr1Counter = 0
+plr2Counter = 0
+
+def checker():
+    global plr2Counter
+    global plr1Counter
+    global plr1
+    global plr2
+
+    #x = 0
+
+    def plr2input():
+        global plr2
+
+        plr2 = int(input("Your response?:"))
+        if plr2 < 100 or plr2 > 999:
+            print("Please enter a number given by the specified range.")
+            plr2input()
     plr2input()
 
     if plr2 == plr1:
@@ -50,19 +94,21 @@ def checker():
         print("Wrong!")
         plr2Counter += 1
         print("Heres your hint, try again.")
-
+        
         if plr2Counter == 1:
-            while x <= 1:
+            x = 0
+            while x < 2:
                 dash[x] = "_"
                 x += 1
                 string = "".join(dash)
             print(string)
+            checker()
         else:
-            while x <= 2:
-                dash[x] = "_"
-                x += 1
-                string = "".join(dash)
+            x = 0
+            dupliDash[x] = "_"
+            string = "".join(dupliDash)
             print(string)
-        checker()
+            checker()
+
 checker()
 #UNDER DEVELOPMENT#
