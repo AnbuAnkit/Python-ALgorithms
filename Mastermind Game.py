8fe27b17349c76515c9fcc2bfc94b3bb91d833e3
Ankit Mahto <138369732+AnbuAnkit@users.noreply.github.com>
2023-11-13 19:26:50 +0530

    Update Mastermind Game.py

--- Mastermind Game.py
+++ Mastermind Game.py
@@ -3,7 +3,7 @@ def intialInput():
     global plr1
     global plr2
 
-    plr1 = 348
+    plr1 = int(input("Please enter a number player 1:"))
     if plr1 < 100 or plr1 > 999:
         print("Please enter a number given by the specified range.")
         intialInput()
@@ -15,51 +15,7 @@ intialInput()
 #DASH CONSTRUCTOR
 strPlr1 = str(plr1)
 dash = []
-
-for i in strPlr1:
-    dash.append(i)
-#########
-
-
-
-#CHECKER FUNCTION
-plr1Counter = 0
-plr2Counter = 0
-
-def checker():
-    global plr2Counter
-    global plr1Counter
-    global plr1
-    global plr2
-
-    x = 0
-
-    def plr2input():
-        global plr2
-
-        plr2 = int(input("Your response?:"))
-        if plr2 < 100 or plr2 > 999:
-            print("Please enter a number given by the specified range.")
-            plr2input()
-    plr2input()#INITIANL INPUT
-def intialInput():
-    global plr1
-    global plr2
-
-    plr1 = 348
-    if plr1 < 100 or plr1 > 999:
-        print("Please enter a number given by the spacified range.")
-        intialInput()
-intialInput()
-#########
-
-
-
-#DASH CONSTRUCTOR
-strPlr1 = str(plr1)
-dash = []
 dupliDash = []
-
 for i in strPlr1:
     dash.append(i)
     dupliDash.append(i)
@@ -70,6 +26,66 @@ for i in strPlr1:
 #CHECKER FUNCTION
 plr1Counter = 0
 plr2Counter = 0
+
+def phase2():
+    def intialInput2():
+        global plr1
+        global plr2
+
+        plr1 = int(input("Please enter a number player 1:"))
+        if plr1 < 100 or plr1 > 999:
+            print("Please enter a number given by the specified range.")
+            intialInput2()
+    intialInput2()
+
+    strPlr2 = str(plr2)
+    dash = []
+    dupliDash = []
+    for i in strPlr2:
+        dash.append(i)
+        dupliDash.append(i)
+
+    def checker2():
+        global plr2Counter
+        global plr1Counter
+        global plr1
+        global plr2
+
+        # x = 0
+
+        def plr2input():
+            global plr2
+
+            plr2 = int(input("Your response?:"))
+            if plr2 < 100 or plr2 > 999:
+                print("Please enter a number given by the specified range.")
+                plr2input()
+
+        plr2input()
+
+        if plr1 == plr2:
+            print("Correct! its player 2's turn now")
+        else:
+            print("Wrong!")
+            plr1Counter += 1
+            print("Heres your hint, try again.")
+
+            if plr1Counter == 1:
+                x = 0
+                while x < 2:
+                    dash[x] = "_"
+                    x += 1
+                    string = "".join(dash)
+                print(string)
+                checker2()
+            else:
+                x = 0
+                dupliDash[x] = "_"
+                string = "".join(dupliDash)
+                print(string)
+                checker2()
+
+    checker2()
 
 def checker():
     global plr2Counter
@@ -89,12 +105,13 @@ def checker():
     plr2input()
 
     if plr2 == plr1:
-        print("Correct!")
+        print("Correct! its player 2's turn now")
+        phase2()
     else:
         print("Wrong!")
         plr2Counter += 1
         print("Heres your hint, try again.")
-        
+
         if plr2Counter == 1:
             x = 0
             while x < 2:
@@ -109,6 +126,5 @@ def checker():
             string = "".join(dupliDash)
             print(string)
             checker()
-
 checker()
 #UNDER DEVELOPMENT#
